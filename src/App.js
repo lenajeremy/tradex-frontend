42b4ad5f68c642814755d4835d98f2cccdf265e5
Jeremiah Lena <jeremiahlena13@gmail.com>
2020-10-10 13:54:21 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,59 +1,16 @@
-import React, { useState, useEffect } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import Header from "./components/Header";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import { Route, Link } from "react-router-dom";
-import { getAllPosts } from "./fetch";
-import { useSelector, useDispatch } from "react-redux";
-import { fetchposts } from "./actions";
 import UserProfile from "./components/UserProfile";
-import { AllPosts, CreatePosts } from "./components/CreateAndViewPosts";
-import Sidebar from './components/SideBar';
+import WrapperFunction from "./components/CreateAndViewPosts";
+import Sidebar from "./components/SideBar";
 
 function App() {
-  // posts from redux store
-  let posts = useSelector((store) => store.posts);
-  let userDetails = useSelector((store) => store.userDetails);
-  const [toLoad, setLoad] = useState(false);
-
-  let dispatch = useDispatch();
-  function updatePosts() {
-    console.log('updatePosts');
-    getAllPosts(lastPost, (data) => {
-      if (data.posts.length !== 0) {
-        dispatch(fetchposts(data.posts));
-        setlastPost(posts.length === 0 ? 2 : posts[posts.length - 1].id + 1);
-      }
-    });
-  }
-  function loadListener(){
-    if(window.scrollY + window.innerHeight >= document.body.offsetHeight){
-      setLoad(true);
-      updatePosts();
-    }
-  }
-  useEffect(() => {
-    updatePosts();
-    window.addEventListener('scroll', loadListener);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(() => {
-    if(toLoad)  {
-      window.removeEventListener('scroll', loadListener);
-      console.log('the event listener has been removed');
-      updatePosts();
-      setLoad(false);
-    }
-  }, [toLoad])
-
-
-  // state of the id of latest post
-  const [lastPost, setlastPost] = useState(1);
-
   return (
     <div className="App">
       <Header />
@@ -62,14 +19,7 @@ function App() {
         <Route
           path="/"
           exact
-          component={() => (
-            <React.Fragment>
-              <h1>Hello {userDetails.userName || "guest"}</h1>
-              <Link to="/user/profile">Profile</Link>
-              <CreatePosts />
-              <AllPosts />
-            </React.Fragment>
-          )}
+          component={WrapperFunction}
         />
         <Route path="/user/profile" component={UserProfile} />
         <Route path="/login" component={Login} />
--- src/components/CreateAndViewPosts.jsx
+++ src/components/CreateAndViewPosts.jsx
@@ -1,47 +1,49 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from "react-redux"
-import { createPost } from '../actions';
+import { createPost, fetchposts } from '../actions';
 import Post from './Post';
-import {createNewPost} from '../fetch';
-import {Redirect} from 'react-router-dom';
+import { createNewPost, getAllPosts } from '../fetch';
+import { Redirect } from 'react-router-dom';
 
 function CreatePosts(props) {
-  const [postContent, setPostContent ] = useState('');
+  const [postContent, setPostContent] = useState('');
   const dispatch = useDispatch();
   const [postImage, setPostImage] = useState(null);
   const user = useSelector(store => store.userDetails)
   const user_id = user.id;
   const [toRedirect, setRedirect] = useState(false);
 
-  function handleFormSubmission(event){
+  function handleFormSubmission(event) {
     event.preventDefault();
-    if(user_id){
-      createNewPost(user_id, postContent, postImage, data =>{
-        if(data.status === 200){
-          console.log('main stuff');
-          dispatch(createPost(data.post_details))
-        }
-      })
-    }else setRedirect(true)
+    user_id ? createNewPost(user_id, postContent, postImage, data => data.status === 200 ? dispatch(createPost(data.post_details)) : setRedirect(true)) : console.error('Error');
   }
-  if(!toRedirect){
+
+  if (!toRedirect) {
     return (
       <React.Fragment>
         <h1>To Create</h1>
-        <form onSubmit ={handleFormSubmission}>
-          <textarea name = 'post_content' value  ={postContent} onChange = {event => setPostContent(event.target.value)} placeholder = {`What's going on?? ${user_id? user.firstName : ''}`}/>
-          <input name = 'postImage' type='file' accept ='image/*' onChange = {event => setPostImage(event.target.files[0])}/>
-          <button type = 'submit'>POST</button>
+        <form onSubmit={handleFormSubmission}>
+          <textarea name='post_content' value={postContent} onChange={event => setPostContent(event.target.value)} placeholder={`What's going on ${user_id ? user.firstName : ''}??`} />
+          <input name='postImage' type='file' accept='image/*' onChange={event => setPostImage(event.target.files[0])} />
+          <button type='submit'>POST</button>
         </form>
       </React.Fragment>
     )
-  }else{
-    return <Redirect to ='/login'/>
+  } else {
+    return <Redirect to='/login' />
   }
 }
 
 function AllPosts(props) {
   const allPosts = useSelector(store => store.posts)
+  const [lastPost, setLastPost] = useState(1);
+  
+  const dispatch = useDispatch();
+  useEffect(() => {
+    getAllPosts(lastPost, data => dispatch(fetchposts(data.posts)));
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  },[]);
+
   return (
     <React.Fragment>
       <h1>Posts</h1>
@@ -51,4 +53,12 @@ function AllPosts(props) {
 
 }
 
-export { AllPosts, CreatePosts }
+function WrapperFunction(props) {
+  return (
+    <React.Fragment>
+      <CreatePosts />
+      <AllPosts />
+    </React.Fragment>
+  )
+}
+export default WrapperFunction
